8bdd5d9733690348905f5b6e8b5b2ebb5c23edcc
Zethu Sanelisiwe Msomi <zemsojhb025@student.wethinkcode.co.za>
2026-07-09 00:01:52 +0200

    feature: created FileAnalyzer class

--- src/main/java/org/example/FileAnalyzer.java
+++ src/main/java/org/example/FileAnalyzer.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+
 public class FileAnalyzer {
+
+    private static final Map<String,Pattern> securityLeaks = new HashMap<>();
+
+    static {
+        // Leak 1: AWS Access Key ID (starts with AKIA followed by 16 characters)
+        securityLeaks.put("AWS Keys:", Pattern.compile("AKIA "));
+
+        // Leak 2: Asymmetric Private Keys (Catches RSA, EC, DSA, or OPENSSH keys)
+        securityLeaks.put("Private Key", Pattern.compile("-----BEGIN (RSA|EC|DSA|OPENSSH) PRIVATE KEY-----"));
+
+        // You can add as many rules here as you want later!
+    }
+
 }
