14284a21f6f240082e5a32ec03ff01f41bf5740f
Zethu Sanelisiwe Msomi <zemsojhb025@student.wethinkcode.co.za>
2026-07-12 23:53:49 +0200

    feature:wrote FileAnalyzer class and the ScanResult class

--- src/main/java/org/leakproof/FileAnalyzer.java
+++ src/main/java/org/leakproof/FileAnalyzer.java
@@ -1,10 +1,11 @@
 package org.leakproof;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
@@ -14,7 +15,7 @@ public class FileAnalyzer {
 
     static {
         // Leak 1: AWS Access Key ID (starts with AKIA followed by 16 characters)
-        securityLeaks.put("AWS Keys:", Pattern.compile("AKIA "));
+        securityLeaks.put("AWS Keys:", Pattern.compile("AKIA[0-9A-Z]{16}"));
 
         // Leak 2: Asymmetric Private Keys (Catches RSA, EC, DSA, or OPENSSH keys)
         securityLeaks.put("Private Key", Pattern.compile("-----BEGIN (RSA|EC|DSA|OPENSSH) PRIVATE KEY-----"));
@@ -22,11 +23,28 @@ public class FileAnalyzer {
         // You can add as many rules here as you want later!
     }
 
-    public List<ScanResult> scanFile(File file) {
+    public List<ScanResult> scanFile(File file){
         List<ScanResult> results = new ArrayList<>();
         int lineNumber = 1;
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null){
+                for(Map.Entry<String, Pattern> entry : securityLeaks.entrySet()){
+                    String entryName = entry.getKey();
+                    Pattern pattern = entry.getValue();
+
+                    Matcher matcher = pattern.matcher(line);
+                    if(matcher.find()){
+                        ScanResult leak = new ScanResult(file, lineNumber, entryName);
+                        results.add(leak);
+                    }
+
+                }
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            System.err.println("Could not find file: " + file.getAbsolutePath());
+        }
         return results;
     }
-
-
 }
--- src/main/java/org/leakproof/Scanner.java
+++ src/main/java/org/leakproof/Scanner.java
@@ -1,4 +1,23 @@
 package org.leakproof;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
+
+    private FileAnalyzer fileAnalyzer;
+    private List<ScanResult> allResults;
+
+    public List<ScanResult> startScan(String targetPath){
+        File filePath = new File(targetPath);
+        List<ScanResult> results = new ArrayList<>();
+
+        if(filePath.exists() && !filePath.isDirectory()) {
+            System.err.println("File is not a directory");
+            return results;
+        }
+        return results;
+    }
+
 }
