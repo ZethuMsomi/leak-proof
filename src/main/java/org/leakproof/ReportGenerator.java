de1aee5e5a70d1627810237ab35dcbc8baed2b85
Zethu Sanelisiwe Msomi <zemsojhb025@student.wethinkcode.co.za>
2026-07-19 20:43:00 +0200

    feature: created report generator

--- src/main/java/org/leakproof/ReportGenerator.java
+++ src/main/java/org/leakproof/ReportGenerator.java
@@ -1,4 +1,26 @@
 package org.leakproof;
 
+import java.util.List;
+
 public class ReportGenerator {
+
+    public void generateReport(List<ScanResult> results){
+
+        if(results.isEmpty()){
+            System.out.println("Scan complete. No leaks found. Great Job!");
+            return;
+        }
+
+        System.out.println("CRITICAL: LEAKS FOUND");
+        System.out.println("Number of leaks found: " + results.size());
+        System.out.println("-----------------------------------------------------");
+
+        for(ScanResult result:results){
+            System.out.println("Leak Type : " + result.getLeakName());
+            System.out.println("Location : " + result.getFile().getAbsolutePath());
+            System.out.println("Line # :" + result.getLineNumber());
+            System.out.println("----------------------------------------------------");
+        }
+    }
 }
+
