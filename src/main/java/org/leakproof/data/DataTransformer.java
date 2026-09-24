76051a762cd00213eba5a6ab2893d79f57076298
Zethu Sanelisiwe Msomi <zemsojhb025@student.wethinkcode.co.za>
2026-09-24 15:02:50 +0200

    created FindingRecord

--- src/main/java/org/leakproof/data/DataTransformer.java
+++ src/main/java/org/leakproof/data/DataTransformer.java
@@ -1,4 +1,35 @@
 package org.leakproof.data;
+import org.leakproof.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataTransformer {
+
+   public List<ScanResult> results;
+   public List<FindingRecord> records;
+
+   public DataTransformer(List<ScanResult> results){
+       this.results = results;
+       this.records = new ArrayList<>();
+   }
+
+   public List<FindingRecord> transform(){
+       for(ScanResult result : results){
+           String severity = "HIGH";
+
+           if(result.getLeakName().equals("Private Key")){
+               severity = "CRITICAL";
+           }
+           FindingRecord record = new FindingRecord(
+                   result.getFile(),
+                   result.getLineNumber(),
+                   result.getLeakName(),
+                   severity
+           );
+
+           records.add(record);
+       }
+       return records;
+   }
 }
